5ea911217d7417d13f4bb2c2d60f6413d5c803d7
Lucas Magalhaes <s2it_lmagalhaes@uolinc.com>
2017-09-29 15:59:37 -0300

    First commit

---
+++ src/main/java/br/com/s2/innersource/innersource/InnersourceApplication.java
@@ -0,0 +1,12 @@
+package br.com.s2.innersource.innersource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnersourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnersourceApplication.class, args);
+	}
+}
